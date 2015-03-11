093a872b3bebd17bb3d44be1a98a0af91fb24044
Ira Auerbach <auerbach.ira@gmail.com>
2015-03-11 13:35:26 -0400

    functional web browser

---
+++ BlocBrowser/WebBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebBrowserViewController.h
+//  BlocBrowser
+//
+//  Created by Ira Auerbach on 3/9/15.
+//  Copyright (c) 2015 Ira Auerbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebBrowserViewController : UIViewController
+
+@end
